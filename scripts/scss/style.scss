7a60b02a25383f1ba07dc450ff68be40f76a00d4
Marc O'Brien <marcobrien12@gmail.com>
2015-11-08 17:45:52 -0800

    Updated scss file

---
+++ scripts/scss/style.scss
@@ -0,0 +1,191 @@
+//colors
+$color_gallery_approx: #eee;
+$white: #fff;
+$color_mine_shaft_approx: #333;
+$black: #000;
+$black_50: rgba(0,0,0,.5);
+$white_37: rgba(255, 255, 255, 0.37);
+$color_black_haze_approx: #f7f7f7;
+$color_nobel_30_approx: rgba(181, 181, 181, 0.3);
+$color_gravel_approx: #474747;
+$color_butterfly_bush_approx: #6f5499;
+$color_shark_approx: #262626;
+
+//urls
+$url_0: url(http://www.jpl.nasa.gov/spaceimages/images/wallpaper/PIA18291-1280x1024.jpg);
+
+//@extend-elements
+//original selectors
+//ul.aerisForecastGrid li, ul.fioForecastGrid li, ul.owmForecastGrid li
+%extend_1 {
+	border: 1px solid $color_gallery_approx;
+	display: inline-block;
+	list-style-type: none;
+	margin-top: 30px;
+}
+
+//original selectors
+//.btn-default, .btn-default:hover, .btn-default:focus
+%extend_2 {
+	color: $color_mine_shaft_approx;
+	//Instead of the line below you could use @include text-shadow($shadow-1, $shadow-2, $shadow-3, $shadow-4, $shadow-5, $shadow-6, $shadow-7, $shadow-8, $shadow-9, $shadow-10)
+	text-shadow: none;
+	background-color: $white;
+	border: 1px solid $white;
+}
+
+//original selectors
+//html, body
+%extend_3 {
+	background: $url_0 no-repeat center center fixed;
+	background-size: cover;
+	height: 100%;
+	background-color: $black;
+	color: $white;
+	text-align: center;
+	//Instead of the line below you could use @include text-shadow($shadow-1, $shadow-2, $shadow-3, $shadow-4, $shadow-5, $shadow-6, $shadow-7, $shadow-8, $shadow-9, $shadow-10)
+	text-shadow: 0 1px 3px $black_50;
+}
+
+//original selectors
+//.btn-scary, .btn-scary:link, .btn-scary:visited
+%extend_4 {
+	background-color: transparent;
+	//Instead of the line below you could use @include border-radius($radius, $vertical-radius)
+	border-radius: 0;
+	color: $white;
+	border: 2px solid $white;
+	height: 50px;
+	width: 120px;
+}
+
+
+ul {
+	&.aerisForecastGrid li {
+		@extend %extend_1;
+	}
+	&.fioForecastGrid li {
+		@extend %extend_1;
+	}
+	&.owmForecastGrid li {
+		@extend %extend_1;
+	}
+}
+#forecast-location {
+	display: none;
+	margin-top: 30px;
+}
+#map-view {
+	min-height: 300px;
+}
+a {
+	color: $white;
+	&:focus {
+		color: $white;
+	}
+	&:hover {
+		color: $white;
+	}
+}
+.btn-default {
+	@extend %extend_2;
+	&:hover {
+		@extend %extend_2;
+	}
+	&:focus {
+		@extend %extend_2;
+	}
+}
+html {
+	@extend %extend_3;
+}
+body {
+	@extend %extend_3;
+}
+.btn-scary {
+	@extend %extend_4;
+	&:link {
+		@extend %extend_4;
+	}
+	&:visited {
+		@extend %extend_4;
+	}
+	&:active {
+		background-color: $color_butterfly_bush_approx;
+		color: $white;
+	}
+	&:hover {
+		background-color: $color_butterfly_bush_approx;
+		color: $white;
+	}
+}
+.box {
+	margin: 100px auto;
+	width: 300px;
+	height: 50px;
+}
+.icon {
+	position: relative;
+	margin-left: -112px;
+	margin-top: 9px;
+	z-index: 1;
+	color: $white;
+}
+.transparent .tuyin {
+	margin-top: 20px;
+	background-color: $white_37;
+	border: 1px solid $color_black_haze_approx;
+	//Instead of the line below you could use @include border-radius($radius, $vertical-radius)
+	border-radius: 5px;
+	//Instead of the line below you could use @include box-shadow($shadow-1, $shadow-2, $shadow-3, $shadow-4, $shadow-5, $shadow-6, $shadow-7, $shadow-8, $shadow-9, $shadow-10)
+	box-shadow: 0 0 2px 0 $color_nobel_30_approx;
+	padding: 20px 40px;
+	text-align: center;
+	width: 97%;
+	min-height: 315px;
+	position: relative;
+	margin-bottom: 25px;
+	.title {
+		text-align: center;
+		color: $white;
+		font-size: 49px;
+	}
+	.plan-name {
+		font-size: 20px;
+		font-weight: 400;
+		border-bottom: 1px solid $white;
+		padding-bottom: 15px;
+		padding: 10px;
+	}
+	.text {
+		margin-top: 20px;
+		font-size: 15px;
+		color: $color_gravel_approx;
+		text-align: center;
+		font-weight: 300;
+		margin-bottom: 13px;
+		font-size: 16px;
+		text-align: justify;
+	}
+}
+input#search-query {
+	width: 610px;
+	height: 50px;
+	background: none;
+	border: 2px solid $white;
+	font-size: 10pt;
+	color: $color_shark_approx;
+	padding-left: 45px;
+	//Instead of the line below you could use @include transition($transition-1, $transition-2, $transition-3, $transition-4, $transition-5, $transition-6, $transition-7, $transition-8, $transition-9, $transition-10)
+	transition: all 0.3s ease-in-out;
+	//Instead of the line below you could use @include transition($transition-1, $transition-2, $transition-3, $transition-4, $transition-5, $transition-6, $transition-7, $transition-8, $transition-9, $transition-10)
+	transition: all .55s ease;
+	&:focus {
+		outline: none;
+		background: $white;
+	}
+	&:active {
+		outline: none;
+		background: $white;
+	}
+}
